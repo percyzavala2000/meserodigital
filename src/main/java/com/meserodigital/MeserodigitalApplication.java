456701b37fed7e3c8bd6fa456b21187d8ea217e2
percyzavala <percyzavala@gmail.com>
2025-06-12 22:35:13 -0500

    Inicio del Proyecto mesero digital

---
+++ src/main/java/com/meserodigital/MeserodigitalApplication.java
@@ -0,0 +1,13 @@
+package com.meserodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeserodigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeserodigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meserodigital/MeserodigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meserodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeserodigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
